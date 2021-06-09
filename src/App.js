0cb7a59aa4f981ab33faa79bef1c96b18883dc60
amindraa05 <60906676+amindraa05@users.noreply.github.com>
2021-06-09 16:57:44 +0700

    chakra-ui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box,Container } from "@chakra-ui/react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Container minWidth="6xl" h="100vh" bg={{base:"red", md:"green",lg:"teal"}} paddingX={{base: "7",md:"20",lg:"20"}}> 
+      <Box bg="tomato" w="100%" p={4} color="white" >
+        This is the Box 
+      </Box>
+    </Container>
+    </>
   );
 }
 
